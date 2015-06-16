ad9ace20ec0c0c69aafd9ac13778377d6d907df8
Mike Smith <neodd70@gmail.com>
2015-06-16 00:15:29 -0400

    Commit after adding a button
    Added button to override the PIR sensor for a set period of time

--- PIR_State/PIR_State.ino
+++ PIR_State/PIR_State.ino
@@ -6,10 +6,10 @@
 
 #include <ESP8266WiFi.h>
 
-const char* ssid     = "*********"; //Enter your local SSID here
-const char* password = "*********"; //Enter your network password here
+const char* ssid     = "not yours"; //Enter your local SSID here
+const char* password = "thenerdherd"; //Enter your network password here
 
-const char* host = "???.???.???.???"; //Enter the IP address of where you want to send the Pin State Data here
+const char* host = "192.168.1.121"; //Enter the IP address of where you want to send the Pin State Data here
 
 void setup() {
   Serial.begin(115200); //Baud rate at which the ESP8266 communicates with the Arduino Terminal
@@ -35,17 +35,58 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   pinMode(2, INPUT); //GPIO2 pin of the ESP8266
+  pinMode(5, INPUT_PULLUP);
 }
 
 int value = 0;
 bool lastState = 0;
 bool PIRState = 0;
+int buttonPin = 5;
 
 void loop() {
-  delay(100);
+  delay(1);
 
   PIRState = digitalRead(2);
   ++value;
+
+
+  if (digitalRead(buttonPin) == 0) {
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 5000;
+    if (!client.connect(host, httpPort)) {
+      Serial.println("connection failed");
+      return;
+    }
+
+    // We now create a URI for the request
+    String url = "/master_bath"; 
+    url += "?button_pressed";
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
+    // This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    delay(10);
+
+    // Read all the lines of the reply from server and print them to Serial
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
+  
+    delay(200);
+  }
+
 
   //Connection to server is made and Pin State transmitted upon Pin State change
   if (PIRState != lastState) {
